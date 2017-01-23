55eac7bfe04ca20f5ea173d2d62755a0a2593e96
denisskin <denis@begom.com>
2017-01-23 03:23:02 +0300

    append filter-methods; add First,Last

--- html_elements.go
+++ html_elements.go
@@ -2,17 +2,33 @@ package httpdoc
 
 type HTMLElements []*HTMLElement
 
-func (ee HTMLElements) String() string {
-	s := "[\n"
-	for _, e := range ee {
-		s += "  " + e.String() + "\n"
+func (ee HTMLElements) String() (s string) {
+	for i, e := range ee {
+		if i > 0 {
+			s += "\n"
+		}
+		s += e.String()
 	}
-	return s + "]"
+	return
 }
 
 func (ee HTMLElements) Eq(num int) *HTMLElement {
 	if num < len(ee) {
 		return ee[num]
+	}
+	return nil
+}
+
+func (ee HTMLElements) First() *HTMLElement {
+	if len(ee) > 0 {
+		return ee[0]
+	}
+	return nil
+}
+
+func (ee HTMLElements) Last() *HTMLElement {
+	if len(ee) > 0 {
+		return ee[len(ee)-1]
 	}
 	return nil
 }
@@ -35,9 +51,18 @@ func (ee HTMLElements) GetByName(id string) *HTMLElement {
 	return nil
 }
 
-func (ee HTMLElements) GetByAttr(name, val string) (res HTMLElements) {
+func (ee HTMLElements) FilterByAttr(attrName string) (res HTMLElements) {
 	for _, e := range ee {
-		if e.Attributes[name] == val {
+		if _, ok := e.Attributes[attrName]; ok {
+			res = append(res, e)
+		}
+	}
+	return
+}
+
+func (ee HTMLElements) FilterByAttrValue(attrName, attrValue string) (res HTMLElements) {
+	for _, e := range ee {
+		if e.Attributes[attrName] == attrValue {
 			res = append(res, e)
 		}
 	}
