297237f6380335eda3d5711276adbe3471ec702c
denis <denis@begom.com>
2019-01-12 10:00:45 -0500

    refactor client

--- client.go
+++ client.go
@@ -9,8 +9,7 @@ import (
 var DefaultClient = NewClient()
 
 func NewClient() *http.Client {
-	jar, err := cookiejar.New(nil)
-	panicOnErr(err)
+	jar, _ := cookiejar.New(nil)
 	return &http.Client{
 		Jar:     jar,
 		Timeout: 60 * time.Second,
